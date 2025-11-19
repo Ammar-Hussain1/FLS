db70ebec5893c1e8d3ce532b1bf67230e2b4a78e
Zoha <zohapasha16@gmail.com>
2025-11-19 20:41:31 +0500

    login page completed

---
+++ WPF/FLS/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfLoginApp
+{
+  
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e) { }
+
+        private void Login_Click(object sender, RoutedEventArgs e) { }
+    }
+}
--- WPF/FLS/obj/Debug/net8.0-windows/FLS.AssemblyInfo.cs
+++ WPF/FLS/obj/Debug/net8.0-windows/FLS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FLS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0735cf93e9bfe05fc53b9a02302a420fd54fd2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc7e1031fa2ecc8485c1f08561232920319361f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FLS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FLS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
